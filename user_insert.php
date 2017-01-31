cdadd266d0d123091ed696fc40a336f4b1fe970e
legoless93 <legoless93@yahoo.co.kr>
2017-01-31 13:57:54 +0000

    Match database
    Edited to match our database

--- user_insert.php
+++ user_insert.php
@@ -7,28 +7,28 @@ include("includes/connection.php");
 
 //mysqli_real_escape_string( connection, the input) will stop the input field from accepting weird names such as code etc.
 
-    $name = $_POST['username'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
     $pass = $_POST['password'];
     $email = $_POST['e_mail'];
-    $country = $_POST['country'];
     $birthday = $_POST['birthday'];
+    $gender = $_POST['gender'];
     //could also use NOW() for the date.
-    $date = date("y-m-d");
-    $status = "unverified";
-    $posts = "No";
+    // $date = date("y-m-d");
+
 
     $get_email = "SELECT * FROM users WHERE user_email = '$email'";
     $run_email = mysqli_query($con, $get_email);
     $check = mysqli_num_rows($run_email);
 
-    if($check == 1) {
+    if($check >= 1) {
     echo "<script>alert('This email is already registered!!!')</script>";
     exit();
   } elseif (strlen($pass) < 8) {
       echo "<script>alert('Password should be 8 characters long')</script>";
       exit();
     } else {
-      $insert = "INSERT INTO users (user_name, user_email, user_pass, user_country, user_birthday, user_image, user_regDate, last_login, status, posts) VALUES ('$name', '$email', '$pass', '$country', '$birthday', 'default.jpg', '$date', '$date', '$status', '$posts')";
+      $insert = "INSERT INTO user (user_email, user_password, user_firstName, user_lastName, user_DoB, user_gender, user_pic) VALUES ('$email', '$pass', '$firstName', '$lastName', '$birthday', '$gender', 'default.jpg')";
       $run_insert = mysqli_query($con, $insert);
 
       if($run_insert) {
