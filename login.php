35f02e393d0aaebe3195c02b0606bb1c405a9523
Ping Ren <ren.ping@hotmail.com>
2017-03-16 17:22:40 +0000

    just play with it

--- login.php
+++ login.php
@@ -5,19 +5,37 @@ if(isset($_POST['login'])) {
   $email = $_POST['logEmail'];
   $pass = $_POST['logPass'];
 
-  $get_user = "SELECT * FROM user WHERE user_email = '$email' AND user_password ='$pass'";
+
+  //add password_verify here
+
+  // $get_user = "SELECT * FROM user WHERE user_email = '$email' AND user_password = '$pass'";
+  $get_user = "SELECT * FROM user WHERE user_email = '$email'";
   $run_user = mysqli_query($con, $get_user);
-  $check = mysqli_num_rows($run_user);
+  $check_email = mysqli_num_rows($run_user);
 
-  if($check == 1) {
+  $rowUsers = mysqli_fetch_array($run_user);
+  $pw_db = $rowUsers['user_password'];
 
-      $rowUsers = mysqli_fetch_array($run_user);
-      $theID = $rowUsers['user_id'];
 
-    $_SESSION['user_email']=$email;
-    echo "<script>window.open('home.php?userid=$theID', '_self')</script>";
-  } else {
-    echo "<script>alert('Email or password is incorrect!!!')</script>";
-  }
+if($check_email == 1){
+if (password_verify($pass, $pw_db)) {
+  // echo "<script>alert($pass)</script>";
+  // echo "<script>alert($test)</script>";
+  $theID = $rowUsers['user_id'];
+
+  $_SESSION['user_email']=$email;
+  // echo "<script>window.open('home.php?userid=$theID', '_self')</script>";
+  // echo "<script type='text/javascript'> document.location = 'home.php?userid=$theID'; </script>";
+    echo "<script type='text/javascript'> document.location = 'Pages/home_feed.php?userid=$theID'; </script>";
+} else {
+  echo "<script>alert('Password is incorrect!!!')</script>";
+  echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
+}
+
+} else {
+  echo "<script>alert('Cannot find email!!!')</script>";
+  echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
+}
+
 }
 ?>
